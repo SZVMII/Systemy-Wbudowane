38805ebb035f5ad69daaa016c8e66dda43424191
SZVMII <92035521+SZVMII@users.noreply.github.com>
2024-03-20 14:41:56 +0100

    Add files via upload

---
+++ lab1kontroler/ISI2_JACHIMOWICZ_ZAD1.X/newmainXC16.c
@@ -0,0 +1,44 @@
+/*
+ * File:   mainXC16.c
+ * Author: local
+ *
+ * Created on March 20, 2024, 2:23 PM
+ */
+
+// CONFIG2
+#pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode selected)
+#pragma config OSCIOFNC = OFF           // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRIPLL           // Oscillator Select (Primary Oscillator with PLL module (HSPLL, ECPLL))
+#pragma config IESO = OFF               // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog Timer is disabled)
+#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+
+#include "xc.h"
+#include <libpic30.h>
+
+int main(void) {
+    
+    unsigned char portValue = 0;
+    char currentS6=0;
+    char currentS7=0;
+    char pastS6=0;
+    char pastS7=0;
+    
+    TRISA = 0x0000;
+    TRISD = 0xFFFF;
+    
+    while(1){
+        LATA = portValue;
+    }
+    
+    return 0;
+}
